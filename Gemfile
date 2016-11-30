7108342b0be5222809f7f6cbba06d7aefb112981
aghyad <aghyad1984@hotmail.com>
2016-11-29 23:05:34 -0600

    migrated to postgresql

--- Gemfile
+++ Gemfile
@@ -3,7 +3,9 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.7'
 # Use mysql as the database for Active Record
-gem 'mysql2', '>= 0.3.13', '< 0.5'
+# gem 'mysql2', '>= 0.3.13', '< 0.5'
+gem 'pg'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
@@ -78,3 +80,5 @@ group :test do
   gem 'selenium-webdriver' , '~> 2.35.1'
   gem 'shoulda-matchers'
 end
+
+gem 'rails_12factor', group: :production
--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,16 @@
 
 ActiveRecord::Schema.define(version: 20161129171245) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
-    t.string   "email",              limit: 255, default: "", null: false
-    t.string   "encrypted_password", limit: 255, default: "", null: false
-    t.datetime "created_at",                                  null: false
-    t.datetime "updated_at",                                  null: false
-    t.string   "provider",           limit: 255
-    t.string   "uid",                limit: 255
+    t.string   "email",              default: "", null: false
+    t.string   "encrypted_password", default: "", null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
