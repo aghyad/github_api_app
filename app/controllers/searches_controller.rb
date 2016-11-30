c3d1faed4709f149a31b5e227e17828a6689a92e
aghyad <aghyad1984@hotmail.com>
2016-11-30 14:34:47 -0600

    refactoring searches controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,64 +1,79 @@
-# require 'will_paginate/array'
 class SearchesController < ApplicationController
   before_action :authenticate_user!
 
   def search
-    if params[:q]
-      params[:page] ||= 1
-      params[:language] ||= ''
-      params[:sort] ||= 'stars'
-      params[:order] ||= 'desc'
+    if search_requested?
+      extract_or_init_search_request_params
 
-      res = search_results
-      if @error
-        flash[:alert] = @error
-        redirect_to home_request_authentication_path unless user_signed_in?
-      else
-        @error = nil
+      ### Search vars
+      @search_keyword = query
+      @page = params[:page]
+      @language_filter = params[:language]
+      @sort = params[:sort]
+      @order = params[:order]
+
+      @language_filters = nil
+      @items = nil
+      @error = nil
+
+      ### Pagination vars (MOVE TO HELPER)
+      @next_url = build_next_url(@search_keyword, @page, @language_filter, @sort, @order)
+      @numeric_pagination_urls = [].tap do |arr|
+        (1..10).each { |num| arr << [num, build_numeric_url(@search_keyword, num, @language_filter, @sort, @order)] }
+      end
+      @previous_url = build_previous_url(@search_keyword, @page, @language_filter, @sort, @order)
+
+      ### Symbols for ordering by "stars" (MOVE TO HELPER)
+      @stars_ordering_url = [
+        (@order=='desc' ? 'Stars [Desc]' : 'Stars [Asc]'),
+        build_numeric_url(@search_keyword, 1, @language_filter, @sort, (@order=='desc' ? 'asc' : 'desc'))
+      ]
+
+      ### now, we do the search:
+      if search_query_present?
+        handler = GithubApiHandler.new(@search_keyword, current_user.email, current_user.encrypted_password, :stars, @order.to_sym, per_page, @page)
+        parsed_response = handler.search_repos
+
+        if parsed_response['ok']
+          parsed_response_items = parsed_response['data']['items'] || []
+          @language_filters = extract_language_filters(parsed_response_items)
+          @items = filter_language(parsed_response_items, @language_filter)
+        else
+          @error = parsed_response['message']
+          flash[:alert] = @error
+          redirect_to home_request_authentication_path unless user_signed_in?
+        end
       end
     end
   end
 
   private
 
-  def search_results
-    per_page = 30
+  def search_requested?
+    !!query
+  end
 
-    @search_keyword = params[:q]
-    @page = params[:page]
-    @language_filter = params[:language]
-    @sort = params[:sort]
-    @order = params[:order]
+  def search_query_present?
+    query.present?
+  end
 
-    @next_url = build_next_url(@search_keyword, @page, @language_filter, @sort, @order)
-    @numeric_pagination_urls = [].tap do |arr|
-      (1..10).each { |num| arr << [num, build_numeric_url(@search_keyword, num, @language_filter, @sort, @order)] }
-    end
-    @previous_url = build_previous_url(@search_keyword, @page, @language_filter, @sort, @order)
+  def any_errors?
+    !!@error
+  end
 
-    @stars_ordering_url = [
-      (@order=='desc' ? 'Stars [Desc]' : 'Stars [Asc]'),
-      build_numeric_url(@search_keyword, 1, @language_filter, @sort, (@order=='desc' ? 'asc' : 'desc'))
-    ]
+  def query
+    params[:q]
+  end
 
-    if @search_keyword.present?
-      handler = GithubApiHandler.new(@search_keyword, current_user.email, current_user.encrypted_password, :stars, @order.to_sym, per_page, @page)
-      parsed_response = handler.search_repos
+  def extract_or_init_search_request_params
+    params[:page] ||= 1
+    params[:language] ||= ''
+    params[:sort] ||= 'stars'
+    params[:order] ||= 'desc'
+  end
 
-      if parsed_response['ok']
-        parsed_response_items = parsed_response['data']['items'] || []
-        @language_filters = extract_language_filters(parsed_response_items)
-        final_items = filter_language(parsed_response_items, @language_filter)
-        @items = final_items #.paginate(page: page, per_page: per_page)
-      else
-        @error = parsed_response['message']
-      end
-
-      # parsed_response_items = parsed_response[:data]['items'] || []
-      # @language_filters = extract_language_filters(parsed_response_items)
-      # final_items = filter_language(parsed_response_items, @language_filter)
-      # @items = final_items
-    end
+  def per_page
+    30
   end
 
   def filter_language(items, language_filter)
